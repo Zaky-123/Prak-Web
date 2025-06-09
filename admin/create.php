10e16001fbb6494677e2b67392022ecb9f1492a0
Zaky-123 <zakymadilaubarus@gmail.com>
2025-06-09 17:20:44 +0700

    create data

--- admin/create.php
+++ admin/create.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Tambah Alumni</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="stylesheet" href="tambah.css">
+    <link rel="stylesheet" href="create.css">
 </head>
 <body>
 <?php
